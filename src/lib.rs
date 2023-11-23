0363a14ab62df7a12d2216e3c6b3f25573856564
Sky <sky@sky9.dev>
2023-11-23 10:51:21 -0500

    documentation!!!
    wowee

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,9 @@ const TAG: usize = 1 << (usize::BITS - 1);
 // every bit except the tag bit
 const MASK: usize = !TAG;
 
+/// `MStr` is a 2-word, immutable version of `Cow<str>`.
+///
+/// See the [crate docs](crate) for more info.
 pub struct MStr<'a> {
     ptr: NonNull<u8>,
     // if high bit (TAG) is set, we are owned
@@ -40,12 +43,69 @@ unsafe impl Sync for MStr<'_> {}
 impl<'a> MStr<'a> {
     // -- Constructors --
 
+    /// Creates a new `MStr<'a>` from an `&'a str`.
+    ///
+    /// The returned `MStr` is borrowed for the same lifetime as the input data.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// # use std::borrow::Cow;
+    /// let s = String::from("foo");
+    /// let mstr = MStr::new_borrowed(&s);
+    ///
+    /// assert!(mstr.is_borrowed());
+    /// assert_eq!(mstr, s);
+    /// assert_eq!(mstr.as_str(), "foo");
+    /// assert_eq!(mstr.as_ptr(), s.as_ptr());
+    /// assert!(matches!(mstr.into_cow(), Cow::Borrowed(_)));
+    /// ```
     #[inline]
     #[must_use]
     pub const fn new_borrowed(s: &'a str) -> MStr<'a> {
         MStr::_new(s.as_ptr(), s.len(), false)
     }
 
+    /// Creates a new `MStr` from owned data.
+    /// The input type is anything that can be converted into a `Box<str>` (String, &str, etc).
+    ///
+    /// The returned `MStr` is owned.
+    /// The lifetime can be chosen to be anything, including `'static`.
+    ///
+    /// If `s` is `Box<str>`, it will not reallocate.  
+    /// If `s` is `String`, it [may reallocate](String::into_boxed_str) if there is excess capacity.  
+    /// If `s` is `&str`, it will be copied to a new heap allocation.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// # use std::borrow::Cow;
+    /// let s = Box::<str>::from("foo");
+    /// let ptr = s.as_ptr();
+    /// let mstr = MStr::new_owned(s);
+    ///
+    /// assert!(mstr.is_owned());
+    /// assert_eq!(mstr, "foo");
+    /// assert_eq!(mstr.as_ptr(), ptr); // the allocation is reused
+    /// assert!(matches!(mstr.into_cow(), Cow::Owned(_)));
+    /// ```
+    ///
+    /// Passing a string slice makes an owned copy:
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let s = "bar";
+    /// let ptr = s.as_ptr();
+    /// let mstr = MStr::new_owned(s);
+    ///
+    /// assert!(mstr.is_owned());
+    /// assert_eq!(mstr, s);
+    /// assert_eq!(mstr, "bar");
+    ///
+    /// // a new allocation was created, and so the pointer are different
+    /// assert_ne!(mstr.as_ptr(), s.as_ptr());
+    /// ```
     #[must_use]
     pub fn new_owned(s: impl Into<Box<str>>) -> MStr<'a> {
         let s = s.into();
@@ -56,6 +116,51 @@ impl<'a> MStr<'a> {
         MStr::_new(ptr, len, true)
     }
 
+    /// Creates a new `MStr<'a>` from a `Cow<'a, str>`.
+    ///
+    /// The returned `MStr` will be borrowed if the cow is borrowed,
+    /// and owned if the cow is owned.  
+    /// If the cow is owned, and has excess capacity, it [may reallocate](String::into_boxed_str).
+    ///
+    /// # Examples
+    ///
+    /// Owned:
+    /// ```rust
+    /// # use mstr::MStr;
+    /// # use std::borrow::Cow;
+    /// let cow = Cow::Owned(String::from("foo"));
+    /// let mstr = MStr::new_cow(cow);
+    ///
+    /// assert!(mstr.is_owned());
+    /// assert_eq!(mstr, "foo");
+    /// assert!(matches!(mstr.into_cow(), Cow::Owned(_)));
+    /// ```
+    ///
+    /// Borrowed:
+    /// ```rust
+    /// # use mstr::MStr;
+    /// # type Cow<'a> = std::borrow::Cow<'a, str>; // fix inference
+    /// let s = String::from("bar");
+    /// let cow = Cow::Borrowed(&s);
+    /// let mstr = MStr::new_cow(cow);
+    ///
+    /// assert!(mstr.is_borrowed());
+    /// assert_eq!(mstr, s);
+    /// assert_eq!(mstr.as_ptr(), s.as_ptr());
+    /// assert!(matches!(mstr.into_cow(), Cow::Borrowed(_)));
+    /// ```
+    ///
+    /// Borrowed (static):
+    /// ```rust
+    /// # use mstr::MStr;
+    /// # use std::borrow::Cow;
+    /// let cow = Cow::Borrowed("qux");
+    /// let mstr = MStr::new_cow(cow);
+    ///
+    /// assert!(mstr.is_borrowed());
+    /// assert_eq!(mstr, "qux");
+    /// assert!(matches!(mstr.into_cow(), Cow::Borrowed("qux")));
+    /// ```
     #[inline]
     #[must_use]
     pub fn new_cow(s: Cow<'a, str>) -> MStr<'a> {
@@ -80,30 +185,120 @@ impl<'a> MStr<'a> {
 
     // -- Accessors --
 
+    /// Converts this `MStr` to a string slice.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let mstr = MStr::new_borrowed("foo");
+    ///
+    /// assert_eq!(mstr.as_str(), "foo");
+    /// ```
     #[inline]
     #[must_use]
     pub const fn as_str(&self) -> &str {
         unsafe { &*self.as_str_ptr() }
     }
 
+    /// Converts this `MStr` to a UTF-8 byte slice.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let mstr = MStr::new_borrowed("foo");
+    ///
+    /// assert_eq!(mstr.as_bytes(), b"foo");
+    /// ```
     #[inline]
     #[must_use]
     pub const fn as_bytes(&self) -> &[u8] {
         self.as_str().as_bytes()
     }
 
+    /// Converts this `MStr` into an owned `String`.
+    /// This will consume `self`.
+    ///
+    /// If `self` is owned, the allocation will be reused.  
+    /// If `self` is borrowed, it will be copied to the heap.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let mstr = MStr::new_borrowed("foo");
+    /// let s: String = mstr.into_string();
+    ///
+    /// assert_eq!(s, "foo");
+    /// ```
+    ///
+    /// Reuses owned allocation:
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let owned = Box::<str>::from("bar");
+    /// let ptr = owned.as_ptr();
+    /// let mstr = MStr::new_owned(owned);
+    /// let s = mstr.into_string();
+    ///
+    /// assert_eq!(s, "bar");
+    /// assert_eq!(s.as_ptr(), ptr);
+    /// ```
     #[inline]
     #[must_use]
     pub fn into_string(self) -> String {
         self.into_cow().into_owned()
     }
 
+    /// Converts this `MStr` into an owned `Box<str>`.
+    /// This will consume `self`.
+    ///
+    /// If `self` is owned, the allocation will be reused.  
+    /// If `self` is borrowed, it will be copied to the heap.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let mstr = MStr::new_borrowed("foo");
+    /// let s: Box<str> = mstr.into_boxed();
+    ///
+    /// assert_eq!(&*s, "foo");
+    /// ```
+    ///
+    /// Reuses owned allocation:
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let owned = Box::<str>::from("bar");
+    /// let ptr = owned.as_ptr();
+    /// let mstr = MStr::new_owned(owned);
+    /// let s = mstr.into_boxed();
+    ///
+    /// assert_eq!(&*s, "bar");
+    /// assert_eq!(s.as_ptr(), ptr);
+    /// ```
     #[inline]
     #[must_use]
     pub fn into_boxed(self) -> Box<str> {
         self.into_string().into_boxed_str()
     }
 
+    /// Converts this `MStr<'a>` into a `Cow<'a, str>`.
+    /// This will consume `self`.
+    ///
+    /// The returned cow will be owned if `self` is owned, and borrowed if `self` is borrowed.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// # use std::borrow::Cow;
+    /// let borrowed = MStr::new_borrowed("foo");
+    /// let owned = MStr::new_owned("bar");
+    ///
+    /// assert!(matches!(borrowed.into_cow(), Cow::Borrowed("foo")));
+    /// assert!(matches!(owned.into_cow(), Cow::Owned(_)));
+    /// ```
     #[must_use]
     pub fn into_cow(self) -> Cow<'a, str> {
         let ptr = self.as_str_ptr();
@@ -118,36 +313,116 @@ impl<'a> MStr<'a> {
         }
     }
 
+    /// Checks if this `MStr` is owned.
+    ///
+    /// The result of this function is mutually exclusive with [`is_borrowed`](MStr::is_borrowed).
+    /// Exactly one of `is_borrowed` and `is_owned` will be true for every `MStr`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let mstr = MStr::new_owned("bar");
+    ///
+    /// assert!(mstr.is_owned());
+    /// assert!(!mstr.is_borrowed());
+    /// ```
     #[inline]
     #[must_use]
     pub const fn is_owned(&self) -> bool {
         self.len & TAG == TAG
     }
 
+    /// Checks if this `MStr` is borrowed.
+    ///
+    /// The result of this function is mutually exclusive with [`is_owned`](MStr::is_owned).
+    /// Exactly one of `is_borrowed` and `is_owned` will be true for every `MStr`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let mstr = MStr::new_borrowed("bar");
+    ///
+    /// assert!(mstr.is_borrowed());
+    /// assert!(!mstr.is_owned());
+    /// ```
     #[inline]
     #[must_use]
     pub const fn is_borrowed(&self) -> bool {
         self.len & TAG == 0
     }
 
+    /// Gets the length of the underlying string slice.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let mstr = MStr::new_borrowed("12345");
+    ///
+    /// assert_eq!(mstr.len(), 5);
+    /// ```
     #[inline]
     #[must_use]
     pub const fn len(&self) -> usize {
         self.len & MASK
     }
 
+    /// Checks if the underlying string slice is empty (length of 0)
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let empty = MStr::new_borrowed("");
+    /// let mstr = MStr::new_borrowed("foo");
+    ///
+    /// assert!(empty.is_empty());
+    /// assert!(!mstr.is_empty());
+    /// ```
     #[inline]
     #[must_use]
     pub const fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Gets a pointer (`*const u8`) to the underlying slice's buffer.
+    ///
+    /// Do **NOT** use the returned pointer mutably, as `self` may be borrowed.
+    ///
+    /// Use [`as_str_ptr`](MStr::as_str_ptr) if you want a `*const str` instead.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let s = "foo";
+    /// let mstr = MStr::new_borrowed(s);
+    ///
+    /// assert_eq!(mstr.as_ptr(), s.as_ptr());
+    /// ```
     #[inline]
     #[must_use]
     pub const fn as_ptr(&self) -> *const u8 {
         self.ptr.as_ptr()
     }
 
+    /// Gets a pointer (`*const str`) to the underlying slice's buffer.
+    ///
+    /// Do **NOT** use the returned pointer mutably, as `self` may be borrowed.
+    ///
+    /// Use [`as_ptr`](MStr::as_ptr) if you want a `*const u8` instead.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let s = "foo";
+    /// let mstr = MStr::new_borrowed(s);
+    ///
+    /// assert_eq!(mstr.as_str_ptr(), s as *const str);
+    /// ```
     #[inline]
     #[must_use]
     pub const fn as_str_ptr(&self) -> *const str {
@@ -158,6 +433,29 @@ impl<'a> MStr<'a> {
 // ===== Trait Impls =====
 
 impl Clone for MStr<'_> {
+    /// Clones this `MStr`.
+    ///
+    /// The returned `MStr` will be owned if `self` is owned, and borrowed if `self` is borrowed.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let mstr = MStr::new_owned("foo");
+    /// let mstr2 = mstr.clone();
+    ///
+    /// assert_eq!(mstr, mstr2);
+    /// ```
+    ///
+    /// Borrowed/Owned is preserved:
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let borrowed = MStr::new_borrowed("bar");
+    /// let owned = MStr::new_owned("qux");
+    ///
+    /// assert!(borrowed.clone().is_borrowed());
+    /// assert!(owned.clone().is_owned());
+    /// ```
     fn clone(&self) -> Self {
         if self.is_borrowed() {
             MStr::_new(self.as_ptr(), self.len(), false)
@@ -179,6 +477,20 @@ impl Drop for MStr<'_> {
 // -- Default --
 
 impl Default for MStr<'_> {
+    /// Creates a new, empty, borrowed `MStr`.
+    ///
+    /// The returned `MStr` can have any lifetime.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use mstr::MStr;
+    /// let default = MStr::default();
+    ///
+    /// assert_eq!(default, "");
+    /// assert!(default.is_empty());
+    /// assert!(default.is_borrowed());
+    /// ```
     fn default() -> Self {
         // dangling
         MStr::new_borrowed(<&str>::default())
