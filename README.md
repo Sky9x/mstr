c3bea5c42303065f589b0eb95eac491ca21c0020
Sky <sky@sky9.dev>
2023-11-22 21:45:06 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# MStr
+
+[![Latest Version]][crates-io]
+[![Crates.io Downloads]][crates-io]
+[![GitHub Stars]][github-com]
+
+[![GitHub]][github-com]
+[![Crates.io]][crates-io]
+[![Docs.rs]][docs-rs]
+
+[Latest Version]: https://img.shields.io/crates/v/mstr?label=version
+[Crates.io Downloads]: https://img.shields.io/crates/d/mstr
+[GitHub Stars]: https://img.shields.io/github/stars/Sky9x/mstr
+[GitHub]: https://img.shields.io/badge/GitHub--white?style=social&logo=github
+[Crates.io]: https://img.shields.io/badge/crates.io--white?style=social&logo=rust
+[Docs.rs]: https://img.shields.io/badge/docs.rs--white?style=social&logo=docs.rs
+
+[crates-io]: https://crates.io/crate/mstr
+[github-com]: https://github.com/Sky9x/mstr
+[docs-rs]: https://docs.rs/mstr
+
+`MStr` is a 2-word, immutable version of `Cow<str>`.
---
+++ src/lib.rs
@@ -0,0 +1,347 @@
+//! [`MStr`] is a 2-word, immutable version of `Cow<str>`.
+
+#![no_std]
+
+extern crate alloc;
+
+use alloc::borrow::Cow;
+use alloc::boxed::Box;
+use alloc::string::String;
+use core::borrow::Borrow;
+use core::cmp::Ordering;
+use core::fmt::{Debug, Display, Formatter, Pointer};
+use core::hash::{Hash, Hasher};
+use core::marker::PhantomData;
+use core::ptr::NonNull;
+use core::{fmt, mem, ptr, str};
+
+// the high bit of usize
+// if set (1), MStr is owned
+// if not (0), MStr is borrowed
+const TAG: usize = 1 << (usize::BITS - 1);
+// every bit except the tag bit
+const MASK: usize = !TAG;
+
+pub struct MStr<'a> {
+    ptr: NonNull<u8>,
+    // if high bit (TAG) is set, we are owned
+    // rust requires all allocations to be less than isize::MAX bytes,
+    // so the top bit is never used and thus available for tagging
+    len: usize,
+
+    _marker1: PhantomData<&'a str>,
+    _marker2: PhantomData<Box<str>>,
+}
+
+unsafe impl Send for MStr<'_> {}
+unsafe impl Sync for MStr<'_> {}
+
+impl<'a> MStr<'a> {
+    // -- Constructors --
+
+    #[inline]
+    #[must_use]
+    pub const fn new_borrowed(s: &'a str) -> MStr<'a> {
+        MStr::_new(s.as_ptr(), s.len(), false)
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn new_owned(s: impl Into<Box<str>>) -> MStr<'a> {
+        let s = s.into();
+
+        let len = s.len();
+        let ptr = Box::into_raw(s).cast::<u8>();
+
+        MStr::_new(ptr, len, true)
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn new_cow(s: Cow<'a, str>) -> MStr<'a> {
+        match s {
+            Cow::Borrowed(s) => MStr::new_borrowed(s),
+            Cow::Owned(s) => MStr::new_owned(s),
+        }
+    }
+
+    #[inline]
+    #[must_use]
+    const fn _new(ptr: *const u8, len: usize, tag: bool) -> MStr<'a> {
+        MStr {
+            // SAFETY: always comes from a valid string
+            ptr: unsafe { NonNull::new_unchecked(ptr.cast_mut()) },
+            len: if tag { len | TAG } else { len },
+
+            _marker1: PhantomData,
+            _marker2: PhantomData,
+        }
+    }
+
+    // -- Accessors --
+
+    #[inline]
+    #[must_use]
+    pub const fn as_str(&self) -> &str {
+        unsafe { &*self.as_str_ptr() }
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn as_bytes(&self) -> &[u8] {
+        self.as_str().as_bytes()
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn into_string(self) -> String {
+        self.into_cow().into_owned()
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn into_boxed(self) -> Box<str> {
+        self.into_string().into_boxed_str()
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn into_cow(self) -> Cow<'a, str> {
+        let ptr = self.as_str_ptr();
+        let is_owned = self.is_owned();
+        mem::forget(self);
+
+        if is_owned {
+            let b = unsafe { Box::from_raw(ptr.cast_mut()) };
+            Cow::Owned(b.into_string())
+        } else {
+            Cow::Borrowed(unsafe { &*ptr })
+        }
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn is_owned(&self) -> bool {
+        self.len & TAG == TAG
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn is_borrowed(&self) -> bool {
+        self.len & TAG == 0
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn len(&self) -> usize {
+        self.len & MASK
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn as_ptr(&self) -> *const u8 {
+        self.ptr.as_ptr()
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn as_str_ptr(&self) -> *const str {
+        ptr::slice_from_raw_parts::<u8>(self.as_ptr(), self.len()) as *const str
+    }
+}
+
+// ===== Trait Impls =====
+
+impl Clone for MStr<'_> {
+    fn clone(&self) -> Self {
+        if self.is_borrowed() {
+            MStr::_new(self.as_ptr(), self.len(), false)
+        } else {
+            MStr::new_owned(self.as_str())
+        }
+    }
+}
+
+impl Drop for MStr<'_> {
+    fn drop(&mut self) {
+        if self.is_owned() {
+            let b = unsafe { Box::from_raw(self.as_str_ptr().cast_mut()) };
+            drop(b);
+        }
+    }
+}
+
+// -- Default --
+
+impl Default for MStr<'_> {
+    fn default() -> Self {
+        // dangling
+        MStr::new_borrowed(<&str>::default())
+    }
+}
+
+// -- Format --
+
+impl Debug for MStr<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        Debug::fmt(self.as_str(), f)
+    }
+}
+
+impl Display for MStr<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        Display::fmt(self.as_str(), f)
+    }
+}
+
+impl Pointer for MStr<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        Pointer::fmt(&self.as_str_ptr(), f)
+    }
+}
+
+// -- Convert From --
+
+impl<'a> From<&'a str> for MStr<'a> {
+    fn from(value: &'a str) -> Self {
+        MStr::new_borrowed(value)
+    }
+}
+
+impl<'a> From<&'a mut str> for MStr<'a> {
+    fn from(value: &'a mut str) -> Self {
+        MStr::new_borrowed(value)
+    }
+}
+
+impl<'a> From<Cow<'a, str>> for MStr<'a> {
+    fn from(value: Cow<'a, str>) -> Self {
+        MStr::new_cow(value)
+    }
+}
+
+impl From<String> for MStr<'_> {
+    fn from(value: String) -> Self {
+        MStr::new_owned(value)
+    }
+}
+
+impl From<Box<str>> for MStr<'_> {
+    fn from(value: Box<str>) -> Self {
+        MStr::new_owned(value)
+    }
+}
+
+// -- Convert To --
+
+impl<'a> From<MStr<'a>> for Cow<'a, str> {
+    fn from(value: MStr<'a>) -> Self {
+        value.into_cow()
+    }
+}
+
+impl From<MStr<'_>> for String {
+    fn from(value: MStr<'_>) -> Self {
+        value.into_string()
+    }
+}
+
+impl From<MStr<'_>> for Box<str> {
+    fn from(value: MStr<'_>) -> Self {
+        value.into_boxed()
+    }
+}
+
+// -- Convert Ref --
+
+// TODO is this a good idea? i think so but
+// impl Deref for MStr<'_> {
+//     type Target = str;
+//
+//     fn deref(&self) -> &Self::Target {
+//         self.as_str()
+//     }
+// }
+
+impl AsRef<str> for MStr<'_> {
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl AsRef<[u8]> for MStr<'_> {
+    fn as_ref(&self) -> &[u8] {
+        self.as_bytes()
+    }
+}
+
+impl Borrow<str> for MStr<'_> {
+    fn borrow(&self) -> &str {
+        self.as_str()
+    }
+}
+
+// no Borrow<[u8]> because str/String don't implement it
+// (because the Hash impls are different)
+
+// -- Hash --
+
+impl Hash for MStr<'_> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        Hash::hash(self.as_str(), state)
+    }
+}
+
+// -- [Partial]Eq --
+
+impl Eq for MStr<'_> {}
+
+impl PartialEq for MStr<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_str() == other.as_str()
+    }
+}
+
+impl PartialEq<str> for MStr<'_> {
+    fn eq(&self, other: &str) -> bool {
+        self.as_str() == other
+    }
+}
+
+impl PartialEq<MStr<'_>> for str {
+    fn eq(&self, other: &MStr<'_>) -> bool {
+        self == other.as_str()
+    }
+}
+
+// -- [Partial]Ord --
+
+impl Ord for MStr<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.as_str().cmp(other.as_str())
+    }
+}
+
+impl PartialOrd for MStr<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialOrd<str> for MStr<'_> {
+    fn partial_cmp(&self, other: &str) -> Option<Ordering> {
+        self.as_str().partial_cmp(other)
+    }
+}
+
+impl PartialOrd<MStr<'_>> for str {
+    fn partial_cmp(&self, other: &MStr<'_>) -> Option<Ordering> {
+        self.partial_cmp(other.as_str())
+    }
+}
